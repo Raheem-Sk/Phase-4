faf72a26894e87ec4b1b811ceba830b203305f91
Raheem-Sk <136781886+Raheem-Sk@users.noreply.github.com>
2023-08-25 14:40:39 +0530

    Add files via upload

---
+++ Typescript/calc.ts
@@ -0,0 +1,16 @@
+let num1=10,num2=20;
+console.log("num1="+num1+" num2="+num2);
+let choice=1;
+switch(choice){
+
+
+    case 1:
+        console.log(num1+" + "+num2 +" = "+ (num1+num2));
+        break;
+    case 2:
+        console.log(num1+" * "+num2 +" = "+ (num1*num2));
+         break;
+    case 3:
+        console.log(num1+" - "+num2 +" = "+ (num1-num2));
+         break;
+}
